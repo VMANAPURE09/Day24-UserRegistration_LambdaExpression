c47fbe93492c3336910312e87779921cad666add
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-17 20:26:45 +0530

    UC2- Check Valide Last Name

--- LambdaUserRegistration/LambdaUserRegistration/UserRegistration.cs
+++ LambdaUserRegistration/LambdaUserRegistration/UserRegistration.cs
@@ -9,20 +9,20 @@ namespace LambdaUserRegistration
 {
     public class UserRegistration
     {
-        public static void CheckFirstName()
+        public static void CheckLastName()
         {
-            Console.WriteLine("Enter your First Name- ");
-            string firstname = Console.ReadLine();
-            string firstnamepattern = @"^[A-Z]{1}[a-z]{2,}$";
+            Console.WriteLine("Enter your Last Name- ");
+            string lastname = Console.ReadLine();
+            string lastnamepattern = @"^[A-Z]{1}[a-z]{2,}$";
 
-            //Validate the first name byusing Lambda Expression  
-            Func<string, bool> Validfirstname = p => Regex.IsMatch(firstname, firstnamepattern);
+            //Validate the Lastname byusing Lambda Expression  
+            Func<string, bool> Validfirstname = p => Regex.IsMatch(lastname, lastnamepattern);
 
-            bool result = Validfirstname(firstname);
+            bool result = Validfirstname(lastname);
             Console.WriteLine(result);
 
 
-
+            
         }
     }
 }
