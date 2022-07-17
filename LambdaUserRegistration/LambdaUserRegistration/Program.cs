66c549c93def5903067fcd2bc644c1b141c2036b
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-17 20:07:32 +0530

    UC1: Enter Valide First Name

---
+++ LambdaUserRegistration/LambdaUserRegistration/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LambdaUserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registrtion Program by using Lambda ");
+            UserRegistration.CheckFirstName();
+        }
+    }
+}
---
+++ LambdaUserRegistration/LambdaUserRegistration/UserRegistration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LambdaUserRegistration
+{
+    public class UserRegistration
+    {
+        public static void CheckFirstName()
+        {
+            Console.WriteLine("Enter your First Name- ");
+            string firstname = Console.ReadLine();
+            string firstnamepattern = @"^[A-Z]{1}[a-z]{2,}$";
+
+            //Validate the first name byusing Lambda Expression  
+            Func<string, bool> Validfirstname = p => Regex.IsMatch(firstname, firstnamepattern);
+
+            bool result = Validfirstname(firstname);
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
